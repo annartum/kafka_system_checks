ae67fd2bbc74b71145b1c2cecdafa87e4da96f95
root <root@edge-shared-1.dilisim.local>
2020-01-30 09:48:36 +0000

    something

--- run-producers.sh
+++ run-producers.sh
@@ -4,6 +4,9 @@ for site in SITE_A SITE_B
 do
   site_address_var=${site}_BROKER_ADDRESS
   export BROKER_ADDRESS=${!site_address_var}
+
+  echo "Creating topics and running producers for ${site}"
+  ./kafka-produce.sh
   echo ${BROKER_ADDRESS}
 done
 

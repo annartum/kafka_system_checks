634395d1ecdf4509c9ef1c23e8d18e0d19e5d7b6
Ismail Keskin <ismail.keskin@dilisim.com>
2020-01-30 13:05:06 +0300

    Per topic element count

--- env.sh
+++ env.sh
@@ -3,9 +3,12 @@ export KAFKA_OPTS="-Djava.security.auth.login.config=jaas.conf -Xmx1G"
 export TOPIC_NAME=ha_test
 export TOPIC_PARTITIONS=6
 export TOPIC_REPLICATION=2
-export TOPIC_PER_PARTITION_ELEMENT_COUNT=1000000
+export TOPIC_PER_PARTITION_ELEMENT_COUNT=10000
 
 export CONSUMER_GROUP_NAME=ha_test_cg
 
 export SITE_A_BROKER_ADDRESS=shared-cdh6-2.dilisim.local:9093
+export SITE_A_COUNTER_INITIAL_VALUE=1
+
 export SITE_B_BROKER_ADDRESS=shared-cdh6-2.dilisim.local:19093
+export SITE_B_COUNTER_INITIAL_VALUE=$(eval ${TOPIC_PER_PARTITION_ELEMENT_COUNT} + 1)
--- run-producers.sh
+++ run-producers.sh
@@ -2,8 +2,8 @@ source env.sh
 
 for site in SITE_A SITE_B
 do
-  site_address_var=${site}_BROKER_ADDRESS
-  export BROKER_ADDRESS=${!site_address_var}
-  echo ${BROKER_ADDRESS}
+   broker_address_var=${site}_BROKER_ADDRESS
+   counter_initial_value_var=${site}_COUNTER_INITIAL_VALUE
+  ./kafka_produce "${!site_address_var}" "${!counter_initial_value_var}"
 done
 

97896957a7866e8f392a75f569a5f502697edf98
Ismail Keskin <ismail.keskin@dilisim.com>
2020-01-30 10:28:29 +0300

    Consumer sysevent logging

--- kafka-consume.sh
+++ kafka-consume.sh
@@ -1,4 +1,4 @@
 source env.sh
 
 kafka-consumer-groups --bootstrap-server ${BROKER_ADDRESS} --topic ${HA_TEST_TOPIC_NAME} --command-config client.properties --group ${HA_TEST_CONSUMER_GROUP_NAME} --reset-offsets --to-earliest
-kafka-console-consumer --bootstrap-server ${BROKER_ADDRESS} --topic ${HA_TEST_TOPIC_NAME} --consumer.config client.properties --property 'print.key=true' --property 'key.separator=:' > kafka_messages.txt
+kafka-console-consumer --bootstrap-server ${BROKER_ADDRESS} --topic ${HA_TEST_TOPIC_NAME} --consumer.config client.properties --enable-systest-events --property 'print.key=true' --property 'key.separator=:' > kafka_messages.txt

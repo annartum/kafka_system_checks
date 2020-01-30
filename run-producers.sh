aaddc464c54d6a8b5b697dad9bd8eae2ad32076f
root <root@edge-shared-1.dilisim.local>
2020-01-30 09:57:49 +0000

    Revert "something"
    This reverts commit ae67fd2bbc74b71145b1c2cecdafa87e4da96f95.

--- run-producers.sh
+++ run-producers.sh
@@ -4,9 +4,6 @@ for site in SITE_A SITE_B
 do
   site_address_var=${site}_BROKER_ADDRESS
   export BROKER_ADDRESS=${!site_address_var}
-
-  echo "Creating topics and running producers for ${site}"
-  ./kafka-produce.sh
   echo ${BROKER_ADDRESS}
 done
 

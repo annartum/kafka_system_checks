9c53f783a1152d105d486b9abd1ef67e813636cc
root <root@edge-shared-1.dilisim.local>
2020-01-30 10:15:58 +0000

    producers

--- kafka-produce.sh
+++ kafka-produce.sh
@@ -2,13 +2,14 @@ source env.sh
 
 BROKER_ADDRESS=$1
 COUNTER_START=$2
+ELEMENT_COUNT=$3
 
 echo "BROKER ADDRESS" $BROKER_ADDRESS
 
 kafka-topics --delete --bootstrap-server ${BROKER_ADDRESS} --command-config client.properties --topic ${TOPIC_NAME}
 kafka-topics --create --bootstrap-server ${BROKER_ADDRESS} --command-config client.properties --topic ${TOPIC_NAME} --partitions ${TOPIC_PARTITIONS} --replication-factor ${TOPIC_REPLICATION}
 kafka-topics --describe --bootstrap-server ${BROKER_ADDRESS} --command-config client.properties --topic ${TOPIC_NAME}
-for i in $(seq ${COUNTER_START} $((${COUNTER_START} + ${TOPIC_PER_PARTITION_ELEMENT_COUNT})) )
+for i in $(seq ${COUNTER_START} $((${COUNTER_START} + ${ELEMENT_COUNT})) )
 do
   for j in $(seq 1 ${TOPIC_PARTITIONS})
   do
--- run-producers.sh
+++ run-producers.sh
@@ -4,6 +4,7 @@ for site in SITE_A SITE_B
 do
    broker_address_var=${site}_BROKER_ADDRESS
    counter_initial_value_var=${site}_COUNTER_INITIAL_VALUE
-  ./kafka-produce.sh "${!broker_address_var}" "${!counter_initial_value_var}"
+   element_count_var=${site}_ELEMENT_COUNT
+  ./kafka-produce.sh "${!broker_address_var}" "${!counter_initial_value_var}" "${!element_count_var}"
 done
 

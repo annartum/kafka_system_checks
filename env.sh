fd0f73121c637bea6663bc1a84d2b8e443d2dbe8
root <root@shared-cdh6-1.dilisim.local>
2020-01-31 12:24:05 +0000

    Max messages

--- env.sh
+++ env.sh
@@ -4,14 +4,13 @@ export TRUSTSTORE_PASSWORD=changeit
 export TOPIC_NAME=ha_test
 export TOPIC_PARTITIONS=6
 export TOPIC_REPLICATION=2
-export TOPIC_PER_PARTITION_ELEMENT_COUNT=100
 
 export CONSUMER_GROUP_NAME=ha_test_cg
 
 export SITE_A_SRM_NAME=kafka1
 export SITE_A_BROKER_ADDRESS=shared-cdh6-2.dilisim.local:9093
 export SITE_A_COUNTER_INITIAL_VALUE=1
-export SITE_A_ELEMENT_COUNT=7
+export SITE_A_ELEMENT_COUNT=17
 
 export SITE_B_SRM_NAME=kafka2
 export SITE_B_BROKER_ADDRESS=shared-cdh6-2.dilisim.local:19093
--- kafka-consume.sh
+++ kafka-consume.sh
@@ -3,13 +3,15 @@ source env.sh
 for site in SITE_A SITE_B
 do
   broker_address_var=${site}_BROKER_ADDRESS
-
+  element_count_var=${site}_ELEMENT_COUNT
+  
   kafka-consumer-groups --bootstrap-server ${!broker_address_var} --topic "${TOPIC_NAME}" --command-config client.properties --group ${CONSUMER_GROUP_NAME} --reset-offsets --to-earliest --execute 
 
   kafka-consumer-groups --bootstrap-server ${!broker_address_var} --topic "${SITE_A_SRM_NAME}.${TOPIC_NAME}" --command-config client.properties --group ${CONSUMER_GROUP_NAME} --reset-offsets --to-earliest --execute
 
   kafka-consumer-groups --bootstrap-server ${!broker_address_var} --topic "${SITE_B_SRM_NAME}.${TOPIC_NAME}" --command-config client.properties --group ${CONSUMER_GROUP_NAME} --reset-offsets --to-earliest --execute
 
-  kafka-console-consumer --bootstrap-server ${!broker_address_var} --whitelist "(^|\.)${TOPIC_NAME}$" \
-        --consumer.config client.properties --timeout-ms 5000 --property 'print.key=true' --property 'key.separator=:' > ${site}_data.txt
+  kafka-console-consumer --bootstrap-server ${!broker_address_var} --whitelist "^((${SITE_A_SRM_NAME}|${SITE_B_SRM_NAME})\.)?${TOPIC_NAME}$" \
+        --consumer.config client.properties --timeout-ms 5000 --property 'print.key=true' --property 'key.separator=:' \
+	--max-messages ${!element_count_var} > ${site}_data.txt
 done
